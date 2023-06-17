4a116a37f04d0f845014ffc25292e670f6993892
Elizaveta Kuskova <lizabet-ka@yandex.ru>
2023-06-18 00:30:49 +0300

    dip: movie 320

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -5,17 +5,20 @@ export default function SearchForm() {
   return (
     <form className='searchForm'>
       <div className='searchForm__box'>
-        <input
-          className='searchForm__input'
-          type='text'
-          placeholder='Фильм'
-          minLength='2'
-          required
-        />
-        <button className='searchForm__btn' type='submit'>
-          </button>
-        <img className='searchForm__tumbl'  src={tumbl}/>
-        <p className='searchForm__shortFilms'>Короткометражки</p>
+        <div className='searchForm__input-box'>
+          <input
+            className='searchForm__input'
+            type='text'
+            placeholder='Фильм'
+            minLength='2'
+            required
+          />
+          <button className='searchForm__btn' type='submit'></button>
+        </div>
+        <div className='searchForm__tumbl-box'>
+          <img className='searchForm__tumbl' src={tumbl} />
+          <p className='searchForm__shortFilms'>Короткометражки</p>
+        </div>
       </div>
     </form>
   );
